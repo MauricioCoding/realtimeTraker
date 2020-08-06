5d62cff2ac2f1228814030473b5e9d321293ff88
Blackobuleto <blackbullet@Blackobuletos-MacBook-Air.local>
2020-08-06 14:05:16 -0600

    realtime contando usuarios
    Aqui socket io esta contando los users pero no hagarra los url

--- src/index.js
+++ src/index.js
@@ -6,16 +6,35 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile('index.html', {root: 'src'}); 
+});
+let counter = 0;
+io.on('connection', function(socket) {
+  console.log( socket.client.conn.server.clientsCount + " users connected" );
+  socket.on('disconnect', function() {
+    console.log( socket.client.conn.server.clientsCount + " users connected" );
+  });
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
-    });
-  });
+
+app.get('/test', (req, res) => {
+  res.sendFile('test.html', {root: 'src'}); 
+});
+
+app.get('/testTwo', (req, res) => {
+  res.sendFile('testTwo.html', {root: 'src'}); 
+});
+
+app.get('/testThree', (req, res) => {
+  res.sendFile('testThree.html', {root: 'src'}); 
+});
+
+
+
 
 http.listen(3018, () => {
-  console.log('listening on *:3000');
+  console.log('listening on *:3018');
 });
+
+
+
